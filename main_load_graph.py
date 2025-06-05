837a1604dc92f14cc8fcb563188173e2b258311d
Merini Hichem <55286812+HichemTTop@users.noreply.github.com>
2025-06-05 12:39:45 +0100

    Add files via upload

---
+++ main_load_graph.py
@@ -0,0 +1,28 @@
+import DataStorage.GraphGenerator as gg
+# from DataExtraction.TwitterExtractor import  TweetExtractor
+import Simulation.Simulator as sim
+import numpy as np
+import pandas as pd
+import time
+from tqdm import  tqdm
+import DataStorage.loadData2DB as fp
+
+print('started')
+Generator=fp.FileUploader()
+
+################ barabasi_albert_graph de taille 100 ############################
+# Generator.uploadGraphToDB('ABS')
+# print("ABS is finiched")
+
+################ barabasi_albert_graph de taille 1000 ############################
+# Generator.uploadGraphToDB('ABM')
+# print("ABM is finiched")
+
+################ barabasi_albert_graph de taille 5000 ############################
+# Generator.uploadGraphToDB('ABL')
+# print("ABL is finiched")
+
+################ facebook_graph de taille 4039 ############################
+Generator.uploadFile(graphModel='FB')
+
+print("fb is finiched")
